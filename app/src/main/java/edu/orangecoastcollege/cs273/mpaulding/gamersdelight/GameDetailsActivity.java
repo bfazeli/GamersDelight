65fd705d6c8eb8b7d4dad7ebc435897897295aa4
bijan102 <bfazeli1@gmail.com>
2016-10-19 20:38:22 -0700

    Completed application.

--- app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/gamersdelight/GameDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/gamersdelight/GameDetailsActivity.java
@@ -1,16 +1,52 @@
 package edu.orangecoastcollege.cs273.mpaulding.gamersdelight;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GameDetailsActivity extends AppCompatActivity {
+
+    private ImageView gameDetailsImageView;
+    private TextView gameDetailsNameTextView, gameDetailsDescriptionTextView;
+    private RatingBar gameDetailsRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_details);
 
+        gameDetailsImageView = (ImageView) findViewById(R.id.gameDetailsImageView);
+        gameDetailsNameTextView = (TextView) findViewById(R.id.gameDetailsNameTextView);
+        gameDetailsDescriptionTextView = (TextView) findViewById(R.id.gameDetailsDescriptionTextView);
+        gameDetailsRatingBar = (RatingBar) findViewById(R.id.gameDetailsRatingBar);
+
         // TODO:  Use the Intent object sent from GameListActivity to populate the Views on
+        Intent intent = getIntent();
+
         // TODO:  this layout, including the TextViews, RatingBar and ImageView with the Game details.
+        AssetManager am = this.getAssets();
+
+        try {
+            InputStream stream = am.open(intent.getStringExtra("ImageName"));
+            Drawable image = Drawable.createFromStream(stream, intent.getStringExtra("Name"));
+            gameDetailsImageView.setImageDrawable(image);
+        } catch (IOException ex)
+        {
+            Log.e("Gamers Delight", "Cannot load image: " + intent.getStringExtra("ImageName") + ex);
+        }
+
+        gameDetailsNameTextView.setText(intent.getStringExtra("Name"));
+        gameDetailsDescriptionTextView.setText(intent.getStringExtra("Description"));
+        gameDetailsRatingBar.setRating(intent.getFloatExtra("Rating", 0));
+
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/gamersdelight/GameListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/mpaulding/gamersdelight/GameListActivity.java
@@ -1,14 +1,20 @@
 package edu.orangecoastcollege.cs273.mpaulding.gamersdelight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 import java.util.List;
 
 import static edu.orangecoastcollege.cs273.mpaulding.gamersdelight.R.id.gameListView;
+import static edu.orangecoastcollege.cs273.mpaulding.gamersdelight.R.id.gameRatingBar;
+import static edu.orangecoastcollege.cs273.mpaulding.gamersdelight.R.id.nameEditText;
 
 public class GameListActivity extends AppCompatActivity {
 
@@ -16,6 +22,8 @@ public class GameListActivity extends AppCompatActivity {
     private List<Game> gamesList;
     private GameListAdapter gamesListAdapter;
     private ListView gameListView;
+    private EditText nameEditText, descriptionEditText;
+    private RatingBar gameRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,9 @@ public class GameListActivity extends AppCompatActivity {
         // TODO:  Connect the ListView with the ListAdapter
         gameListView = (ListView) findViewById(R.id.gameListView);
         gameListView.setAdapter(gamesListAdapter);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        descriptionEditText = (EditText) findViewById(R.id.descriptionEditText);
+        gameRatingBar = (RatingBar) findViewById(R.id.gameRatingBar);
     }
 
     public void viewGameDetails(View view) {
@@ -45,20 +56,49 @@ public class GameListActivity extends AppCompatActivity {
         // TODO: Use an Intent to start the GameDetailsActivity with the data it needs to correctly inflate its views.
         if (view instanceof LinearLayout)
         {
-            LinearLayout
+            LinearLayout selectedGameLinearLayout = (LinearLayout) view;
+            Game selectedGame = (Game) selectedGameLinearLayout.getTag();
+
+            Intent detailIntent = new Intent(this, GameDetailsActivity.class);
+            detailIntent.putExtra("Name", selectedGame.getName());
+            detailIntent.putExtra("Description", selectedGame.getDescription());
+            detailIntent.putExtra("Rating", selectedGame.getRating());
+            detailIntent.putExtra("ImageName", selectedGame.getImageName());
+            startActivity(detailIntent);
         }
     }
 
     public void addGame(View view)
     {
         // TODO:  Add a game to the database, list, list adapter
-        // TODO:  Make sure the list adapter is updated
-        // TODO:  Clear all entries the user made (edit text and rating bar)
+        String nameText = nameEditText.getText().toString();
+        String description = descriptionEditText.getText().toString();
+        float rating = gameRatingBar.getRating();
+
+        if (nameText.isEmpty())
+        {
+            Toast.makeText(this, "Game name cannot be empty.", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Game newGame = new Game(nameText, description, rating);
+            // TODO:  Make sure the list adapter is updated
+            gamesListAdapter.add(newGame);
+            db.addGame(newGame);
+
+            // TODO:  Clear all entries the user made (edit text and rating bar)
+            nameEditText.setText("");
+            descriptionEditText.setText("");
+            gameRatingBar.setRating(0);
+        }
     }
 
     public void clearAllGames(View view)
     {
         // TODO:  Delete all games from the database and lists
+        gamesList.clear();
+        db.deleteAllGames();
+        gamesListAdapter.notifyDataSetChanged();
 
     }
 
